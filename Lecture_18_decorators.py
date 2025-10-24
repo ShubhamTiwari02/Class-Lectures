a03e59654729e080d9f2258681852103db98dd62
ShubhamTiwari02 <51123358+ShubhamTiwari02@users.noreply.github.com>
2025-10-24 20:42:05 +0530

    Add files via upload

--- Lecture_18_decorators.py
+++ Lecture_18_decorators.py
@@ -19,7 +19,7 @@ print(random.randint(1,10000))'''
 def my_name():
     return "hello raGhav, how Are yoU?"
 
-print(my_name())  '''
+print(my_name())  
 
 def change_Uppercase(arg):
     def inner_func():
@@ -46,3 +46,75 @@ def my_Uppername1():
 print(my_Lowername()) 
 print(my_Uppername())
 print(my_Uppername1())
+
+#Multiple decorators
+
+def greet_decorator(arg):
+    def inner_func1():
+        #print("Hello", arg())
+        return "Hello! " + arg() + " Have a nice day."  
+    return inner_func1
+
+def change_Uppercase(arg):
+    def inner_func2():
+        return arg().upper()
+    return inner_func2
+
+@greet_decorator
+@change_Uppercase
+def my_greeting():
+    return "As-salamu alaykum, SHubHAM TIwarI here."
+
+
+@greet_decorator
+def your_greeting():
+    return "As-salamu alaykum, SHubHAM TIwarI here."
+
+print(my_greeting()) 
+print(your_greeting())
+
+#Decorator with arguments
+def changcase(n):
+    def change_Uppercase(arg):
+        def inner_func():
+            if n == 1:
+                return arg().upper(),n 
+            elif n == 2:
+                return arg().lower(),n
+            else:
+                return arg()
+        return inner_func
+    return change_Uppercase
+
+@changcase(1)
+def my_name():
+    return "hello raGhav, how Are yoU?"
+
+print(my_name())
+
+def changcase(func):
+    def my_inner(name):
+        return func(name.upper())
+    return my_inner
+
+@changcase
+def my_function(abc, xyz, pqr):
+    return f"Hello, {abc}, {xyz}, and {pqr}! how ARE you doing today?"
+
+print(my_function("Raghav", "Shubham", "Anjali")) 
+print(my_function("Shubham"))
+print(my_function("Anjali"))
+print(my_function("Priya"))'''
+
+def changecase(func):
+    def my_inner(*args):
+        return func(*args).upper()
+    return my_inner
+
+@changecase
+def my_function(*args):
+    return f"Hello, {', '.join(args)}! how ARE you doing today?"
+print(my_function("Raghav", "Shubham"))
+print(my_function("Anjali", "Priya"))
+print(my_function("Sonia", "Monica", "Rachel"))
+print(my_function("Chandler", "Joey", "Ross", "Phoebe"))
