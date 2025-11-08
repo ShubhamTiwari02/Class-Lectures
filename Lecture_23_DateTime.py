3bbc48c1d84073fb74b182c3f3b950cf2d31884f
ShubhamTiwari02 <51123358+ShubhamTiwari02@users.noreply.github.com>
2025-11-08 20:59:26 +0530

    Add files via upload

---
+++ Lecture_23_DateTime.py
@@ -0,0 +1,28 @@
+#datetime
+import datetime
+n = datetime.datetime.now()
+#print("Current date and time:", n)
+#print("Current year:", n.year)
+#print("Current month:", n.month)
+#print("Current day:", n.day)
+#print("Current hour:", n.hour)
+#print("Current minute:", n.minute)
+#print("Current second:", n.second)
+#print(n.strftime("%D"))
+#print(n.strftime("%Y-%m-%d %H:%M:%S"))
+#print(n.strftime("%W"))
+#print(n.strftime("%I:%M %p"))  # 12-hour format with AM/PM
+#print(n.strftime("%H:%M %p"))  # 24-hour format with AM/PM
+#print(n.strftime("%B %d, %Y"))
+#print(n.strftime("%A, %B %d, %Y"))
+
+#x = datetime.datetime(2005, 7, 8)
+#print(x.strftime("%A, %B %d, %Y"))
+
+#print(n.strftime("%c"))  # Day of the year
+#print(n.strftime("%x"))
+#print(n.strftime("%X"))
+#print(n.strftime("%V"))  # ISO week number of the year
+#print(n.strftime("%C"))  # Century (year divided by 100, truncated to an integer)
+#print(n.strftime("%f"))  # Microsecond as a zero-padded decimal number
+#print(n.strftime("%Z"))  # Time zone name
