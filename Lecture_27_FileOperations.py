bfaf280b9a50b481ed5ccc56b9fefddaabb28bb3
ShubhamTiwari02 <51123358+ShubhamTiwari02@users.noreply.github.com>
2025-12-04 19:15:27 +0530

    Add files via upload

--- Lecture_27_FileOperations.py
+++ Lecture_27_FileOperations.py
@@ -70,4 +70,32 @@
 # else:
 #     print("The file does not exist")
 #os.rmdir("myfolder")  # Delete the folder named myfolder
+#import os
+#import shutil
+#os.rename("renamed_sample.txt", "sample.txt")  # Rename the file named sample1.txt to renamed_sample.txt
+#print("updated the name successfully")
+#os.makedirs("myfolder/myfolder1")  # Create a folder named myfolder and a subfolder named myfolder1
+#print("folder created successfully")
+#os.getcwd()  # Get the current working directory
+#os.chdir("c:\\Users\\eswtiuh\\Downloads\\HR and LC\\Tutorials")  # Change the current working directory to myfolder
+#os.rmdir("myfolder/myfolder1")  # Delete the folder named myfolder1
+#shutil.rmtree("myfolder")  # Delete the folder named myfolder and all its contents
+with open("sample2.txt", "w") as file:  # Open a file in write mode
+    file.write("Hello, World!")  # Write a new line to the file
+    file.write("\nThis is a sample file.")  # Write another new line to the file
+    file.write("\nFile handling in Python is easy.")  # Write another new line to the file
+    file.write("\nGoodbye!")  # Write another new line to the file
+    file.write("\nEnd of file.")  # Write another new line to the file
+    file.write("\nHave a nice day!")  # Write another new line to the file
+    file.write("\nSee you soon!")  # Write another new line to the file
+    file.write("\nTake care!")  # Write another new line to the file
+    file.write("\nStay safe!")  # Write another new line to the file
+    file.write("\nHappy coding!")  # Write another new line to the file
+    #file.flush()  # Flush the internal buffer to the file
+    #print("Data flushed to the file successfully.")
 
+
+with open("sample2.txt", "w") as file:  # Open a file in append mode
+    file.flush()  # Flush the internal buffer to the file
+
+#print("Data flushed to the file successfully.")
